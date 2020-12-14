6cb39e6668771b6267e069845d8a704a62ce0bfe
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 10:37:43 +0000

    added comments to clarify process of formatting launch years

--- src/containers/LaunchContainer.js
+++ src/containers/LaunchContainer.js
@@ -18,14 +18,14 @@ const LaunchContainer = () => {
     res2
       .json()
       .then(function (res2) {
-        const manyDates = res2.map(({ launch_year }) => launch_year);
+        const manyDates = res2.map(({ launch_year }) => launch_year); ///this turns the array of key-value pairs into just an array of the launch year values
         return manyDates;
       })
       .then(function (manyDates) {
-        const newSet = [...new Set(manyDates)];
+        const newSet = [...new Set(manyDates)]; //this makes an array of only unique dates with no repetition
         return newSet;
       })
-      .then((newSet) => setAllLaunchYears(newSet))
+      .then((newSet) => setAllLaunchYears(newSet)) //this sets the LaunchYears to the unique set of launch years
       .then(() => setLoading(false))
       .catch((errors) => setErrors(errors));
   }
