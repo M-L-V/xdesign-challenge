0c1d12299dfafb30e227914180ecb5bbd15df30b
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 13:44:07 +0000

    added basic listcontainer component to wrap launchescontainer

--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -4,9 +4,16 @@ import LaunchContainer from "./LaunchContainer";
 class ListContainer extends Component {
   render() {
     return (
-      <div>
-        <img src={"/assets/img/launch-home.png"} />
-        <LaunchContainer />
+      <div className="launch-container">
+        <div className="launch-container__images">
+          <div className="launch-container__images_container">
+            <img src={"/assets/img/spacex-logo.png"} className="logo" />
+            <img src={"/assets/img/launch-home.png"} className="rocket-image" />
+          </div>
+        </div>
+        <div className="launch-container__launches">
+          <LaunchContainer />
+        </div>
       </div>
     );
   }
