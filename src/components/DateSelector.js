978bc28ce771cae27dc46cdc05e3de9f3cdf4a2f
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 10:36:14 +0000

    fetched launch years from API and formatted before placing in state of allLaunch years

---
+++ src/components/DateSelector.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const DateSelector = () => {};
+
+export default DateSelector;
--- src/containers/LaunchContainer.js
+++ src/containers/LaunchContainer.js
@@ -11,6 +11,23 @@ const LaunchContainer = () => {
   async function fetchAllLaunches() {
     const res1 = await fetch("https://api.spacexdata.com/v3/launches");
     res1.json().then((res1) => setLaunches(res1));
+
+    const res2 = await fetch(
+      "https://api.spacexdata.com/v3/launches?filter=launch_year"
+    );
+    res2
+      .json()
+      .then(function (res2) {
+        const manyDates = res2.map(({ launch_year }) => launch_year);
+        return manyDates;
+      })
+      .then(function (manyDates) {
+        const newSet = [...new Set(manyDates)];
+        return newSet;
+      })
+      .then((newSet) => setAllLaunchYears(newSet))
+      .then(() => setLoading(false))
+      .catch((errors) => setErrors(errors));
   }
 
   useEffect(() => {
