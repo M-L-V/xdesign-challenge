bf1bbd7ca55a0ebbf9477f3ded1a57a104e48d76
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 10:48:48 +0000

    added translate date functionality to launch item

--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -3,6 +3,18 @@ import React from "react";
 const LaunchItem = (props) => {
   if (!props.launch) return null;
 
+  const translateDate = () => {
+    const event = new Date(props.launch.launch_date_utc);
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const translatedDate = event.toLocaleDateString(undefined, options);
+    return translatedDate;
+  };
+
   return (
     <div>
       <h1>{props.launch.mission_name}</h1>
