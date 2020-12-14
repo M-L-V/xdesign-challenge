52985d7a94781541d0cb113a0dd18d696da2bdd5
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 18:06:50 +0000

    changed launch item date to short version

--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -8,7 +8,7 @@ const LaunchItem = (props) => {
     const options = {
       weekday: "long",
       year: "numeric",
-      month: "long",
+      month: "short",
       day: "numeric",
     };
     const translatedDate = event.toLocaleDateString(undefined, options);
@@ -20,9 +20,9 @@ const LaunchItem = (props) => {
       <div className="launch-item__wrapper">
         <h1> #{props.launch.flight_number} </h1>
         <h2>{props.launch.mission_name}</h2>
-        <div>
+        <div className="launch-item__date">
           <p> {translateDate(props.launch.launch_date_utc)}</p>
-          <p> {props.launch.rocket.rocket_name} </p>
+          <h3> {props.launch.rocket.rocket_name} </h3>
         </div>
       </div>
     </div>
