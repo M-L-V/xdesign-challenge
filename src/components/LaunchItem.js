ed5898eea193ee5540bd9bf6e7156c59160d8b0d
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-14 14:24:25 +0000

    added rocket name to launch item

--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -12,13 +12,15 @@ const LaunchItem = (props) => {
       day: "numeric",
     };
     const translatedDate = event.toLocaleDateString(undefined, options);
+    console.log("rocket is", props.launch.rocket);
     return translatedDate;
   };
 
   return (
-    <div>
+    <div className="launch-item">
+      <p> #{props.launch.flight_number} </p>
       <h1>{props.launch.mission_name}</h1>
-      <p> {props.launch.flight_number} </p>
+      <p> {props.launch.rocket.rocket_name} </p>
       <p> {translateDate(props.launch.launch_date_utc)}</p>
     </div>
   );
