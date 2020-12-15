933eebc849d5f2c60c6daacea663cde7ea346ee5
m-l-v <53375046+M-L-V@users.noreply.github.com>
2020-12-15 11:33:36 +0000

    removed unused imports from app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.scss";
 
-import LaunchContainer from "./containers/LaunchContainer";
 import ListContainer from "./containers/ListContainer";
 
 function App() {
   return (
     <div className="App">
-      {/* <LaunchContainer /> */}
       <ListContainer />
     </div>
   );
